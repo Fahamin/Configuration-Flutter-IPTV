8ef282b1dd7bc29ae7ed224c8f538381ea051c43
Fahamin <fahamin00@gmail.com>
2023-05-21 18:12:30 +0600

    futur try

--- lib/pages/player_page.dart
+++ lib/pages/player_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
-
 import '../DB/sqfliteHelper.dart';
 import '../ProviderHandaler/ProviderHandle.dart';
 
@@ -13,24 +12,24 @@ class PlayerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder<List>(
-        future: SQLHelper.getChannelByCategory(Get.arguments),
-        builder: (context, snapshot) {
-          return snapshot.hasData
-              ? ListView.builder(
-                  itemCount: snapshot.data?.length,
-                  itemBuilder: (_, int position) {
-                    return Card(
-                      child: ListTile(
-                        title: Text(snapshot.data?[position]['title']),
-                      ),
-                    );
-                  },
-                )
-              : Center(
-                  child: CircularProgressIndicator(),
-                );
-        },
+      body: Container(
+        child: FutureBuilder(
+          future: SQLHelper.getChannelByCategory(Get.arguments),
+          builder: (context, snapshot) {
+            return snapshot.hasData
+                ? ListView.builder(
+                    itemCount: snapshot.data?.length,
+                    itemBuilder: (_, int position) {
+                      return Card(
+                        child: ListTile(
+                          title: Text(snapshot.data?[position]['title']),
+                        ),
+                      );
+                    },
+                  )
+                : Center(child: CircularProgressIndicator(),);
+          },
+        ),
       ),
     );
   }
